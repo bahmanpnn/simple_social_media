4fe1ba00e2260404c63a947d3ab5af072d6b4a12
Bahman Pournazari <bahmanpn@gmail.com>
2024-05-17 19:48:17 +0330

    9- overrided clean too check password with confirm password for validating

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,21 +12,46 @@ class RegisterForm(forms.Form):
     email=forms.EmailField(widget=forms.EmailInput(attrs={
         'class':'form-control col-md-3',
         'placeholder':'email',
-        'required':False
     }))
 
     password=forms.CharField(widget=forms.PasswordInput(attrs={
         'class':'form-control col-md-3',
         'placeholder':'password'
     }))
+    confirm_password=forms.CharField(widget=forms.PasswordInput(attrs={
+        'class':'form-control col-md-3',
+        'placeholder':'confirm password'
+    }))
 
 
     def clean_email(self):
         email=self.cleaned_data['email']
-        user=User.objects.filter(email=email).exists()
+        if email=='':
+            raise ValidationError('please fill email field too!!')
         
+        user=User.objects.filter(email=email).exists()
+
         if user:
             raise ValidationError('this email used before!! please register again with another email :))')
         
         return email
+    
+    def clean_username(self):
+        username=self.cleaned_data['username']
+        user=User.objects.filter(username=username).exists()
+
+        if user:
+            raise ValidationError('this username exists and used before!! please use another username')
+        
+        return username
+    
+
+    def clean(self):
+        cd=super().clean()
+
+        password=cd.get('password')
+        confirm_password=cd.get('confirm_password')
+
+        if password and confirm_password and password !=confirm_password:
+            raise ValidationError('password with confirm password does not match!! check again please')
     
